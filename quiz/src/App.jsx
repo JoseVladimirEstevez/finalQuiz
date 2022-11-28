4fa7effc15cb6556b1cdd472c18a067f06767243
vlad <estevez.vladimir@yahoo.com>
2022-11-28 11:51:42 -0500

    first changes to App

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,19 +1,17 @@
 function App() {
   return (
-    <div className="bg-light">
-      <header className="text-center">
-        <p>
-          Welcome to my app!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{minHeight:"100vh"}} className="d-flex p-5 text-center bg-light  justify-content-center align-items-center ">
+      <div className="">
+        <h1>
+          Welcome to Quiz Application!
+        </h1>
+        <h5  class=" py-5 ">Made by Vladimir and Munir.       
+        </h5>
+        <div>
+        <button type="button" class="px-5 py-3 btn btn-lg btn-secondary">Play</button>
+
+        </div>
+      </div>
     </div>
   );
 }
