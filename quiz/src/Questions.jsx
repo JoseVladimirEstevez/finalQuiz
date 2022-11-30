62d40b9db1f867a2290a08034168277252c48fc7
vlad <estevez.vladimir@yahoo.com>
2022-11-30 10:53:47 -0500

    commented all the props

--- quiz/src/Questions.jsx
+++ quiz/src/Questions.jsx
@@ -3,11 +3,9 @@ import OptionButton from './OptionButton'
 import {Link} from 'react-router-dom'
 
 function Questions(props) {
-  return (
+  return(<>  
   
-  <div>
-    
-    
+ <div> 
     <div className=" " >Q 1/10<div style={{minWidt:"100px"}}>
    <Link className="d-flex justify-content-end " to="/">  <button> STOP</button> </Link> 
    </div></div>
@@ -17,28 +15,29 @@ function Questions(props) {
   <div  className="d-flex p-3 text-center  justify-content-center align-items-center ">
       <div className="">
         <h1>
-     {props.category=""}   Quiz
+     {/*{props.category=""}*/}   Quiz
         </h1>
         <h5  class=" py-3 ">
-            {props.question=""}
+            {/*{props.question=""}*/}
         </h5>
         
       </div>
     </div>
 
   <div className="row">
-    <OptionButton name = {props.correctAnswer}></OptionButton>
-    <OptionButton name = {props.incorrectAnswer[0]}></OptionButton>
+    <OptionButton  >{/*name ={props.correctAnswer}}*/}</OptionButton>
+    <OptionButton  >{/*name ={props.incorrectAnswer[0]}}*/}</OptionButton>
   </div>
   <div className="row">
-  <OptionButton name = {props.incorrectAnswer[1]}></OptionButton>
-  <OptionButton name = {props.incorrectAnswer[2]}></OptionButton>
+  <OptionButton > {/* name ={props.incorrectAnswer[1]}}*/}</OptionButton>
+  <OptionButton > {/* name ={props.incorrectAnswer[2]}}*/}</OptionButton>
       
   </div>
   
 </div>
 </div>
-  )
+
+</>  )
 }
 
 export default Questions
