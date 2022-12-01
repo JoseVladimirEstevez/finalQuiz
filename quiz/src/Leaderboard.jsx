e2ae831f4d5b4bf8008c7fc567bba503dc4c8f18
Munir Khaliqyar <le2176022@crc-lennox.qc.ca>
2022-12-01 13:15:47 -0500

    Adjust the table in Leaderboard component

--- quiz/src/Leaderboard.jsx
+++ quiz/src/Leaderboard.jsx
@@ -1,24 +1,41 @@
 import React from 'react'
 import Row from './components/Row'
+import { Link } from 'react-router-dom'
 
 function leaderboard() {
-  const arr = {name: 'afd', category: 'afda', score: '2'};
+  const arr = [{name: 'afd', category: 'afda', score: '2'}];
 
+  let data = [];
+  
+  for(let i = 0; i < 10; i++){
+    if(arr[i] != null){
+      data.push(arr[i]);
+    }
+    else{
+      data.push({name:' ', category:' ', score:' '});
+    }
+  }
   return (
     <div>
       <div className="container">
         <div className="row text-center m-5">
-          <div className="col align-items-center"><h1>Top Scores</h1></div>
-          <div className="col"><button   type="button" className="btn btn-lg btn-secondary align-items-end">Replay</button></div>
+          <div className="col align-items-center"><h1 className='position-absolute start-50'>Top Scores</h1></div>
+          <div className="col"><Link to='../selectCategory'><button   type="button" className="btn btn-lg btn-secondary p-3 px-5 position-absolute top-0 end-0 m-5">Replay</button></Link></div>
         </div>
         <div className="row text-center align-items-center">
-          <table>
-            <tr>
-              <th>Name</th>
-              <th>Category</th>
-              <th>Score</th>
-            </tr>
-            <Row name={arr.name} category={arr.category} score={arr.score}></Row>
+          <table className='m-5 table table-center'>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Score</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item, index) => {
+                return <Row name={item.name} category={item.category} score={item.score} key={index}></Row>
+              })}
+            </tbody>
          </table>
         </div>
       </div>
