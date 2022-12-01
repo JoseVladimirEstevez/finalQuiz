d84a7fed7f14c4540215f7ab50a0a8c0c1d0a42b
Munir Khaliqyar <le2176022@crc-lennox.qc.ca>
2022-11-30 22:43:17 -0500

    modify the code of Leaderboard component

--- quiz/src/Leaderboard.jsx
+++ quiz/src/Leaderboard.jsx
@@ -7,18 +7,18 @@ function leaderboard() {
   return (
     <div>
       <div className="container">
-        <div className="row text-center">
-          <div className="col"><h1>Top Scores</h1></div>
-          <div className="col"><button   type="button" className="btn btn-lg btn-secondary">Replay</button></div>
+        <div className="row text-center m-5">
+          <div className="col align-items-center"><h1>Top Scores</h1></div>
+          <div className="col"><button   type="button" className="btn btn-lg btn-secondary align-items-end">Replay</button></div>
         </div>
-        <div className="row">
+        <div className="row text-center align-items-center">
           <table>
-         <tr>
-          <th>Name</th>
-          <th>Category</th>
-          <th>Score</th>
-         </tr>
-          <Row name={arr.name} category={arr.category} score={arr.score}></Row>
+            <tr>
+              <th>Name</th>
+              <th>Category</th>
+              <th>Score</th>
+            </tr>
+            <Row name={arr.name} category={arr.category} score={arr.score}></Row>
          </table>
         </div>
       </div>
