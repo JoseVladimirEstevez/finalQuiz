a92bfd246465873809cf6aabd5aea23b1704519a
Munir Khaliqyar <le2176022@crc-lennox.qc.ca>
2022-12-01 13:14:21 -0500

    Change the css style of table row and remove the div tag from Row component

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -2,14 +2,14 @@ import {Link} from 'react-router-dom'
 function App() {
   return (
     <div style={{minHeight:"100vh"}} className="d-flex p-5 text-center  justify-content-center align-items-center ">
-      <div className="">
+      <div>
         <h1>
           Welcome to Quiz Application!
         </h1>
-        <h5  class=" py-5 ">Made by Vladimir and Munir.       
+        <h5  className=" py-5 ">Made by Vladimir and Munir.       
         </h5>
         <div> 
-        <Link style={{color:"inherit",textDecoration:"none"}} to="/selectCategory"> <button   type="button" class="px-5 py-3 btn btn-lg btn-secondary">Play </button></Link>
+        <Link style={{color:"inherit",textDecoration:"none"}} to="/selectCategory"> <button   type="button" className="px-5 py-3 btn btn-lg btn-secondary">Play </button></Link>
         </div>
       </div>
     </div>
--- quiz/src/components/Row.jsx
+++ quiz/src/components/Row.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 
 function row(props) {
   return (
-    <div>
+    <>
         <tr>
             <td>{props.name}</td>
             <td>{props.category}</td>
             <td>{props.score}</td>
         </tr>
-    </div>
+    </>
   )
 }
 
