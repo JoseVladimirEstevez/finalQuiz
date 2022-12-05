cc42b2f38e3d65a53cea3e6f2345b19fd7a70e16
Munir Khaliqyar <le2176022@crc-lennox.qc.ca>
2022-12-05 12:35:08 -0500

    create a constant.js to assign the constant values inside it

---
+++ quiz/src/constants.js
@@ -0,0 +1,3 @@
+export const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiTXVuaXIiLCJzdHVkZW50SUQiOiIyMTc2MDIyIiwiaWF0IjoxNjcwMjU2MjQ4LCJleHAiOjE2NzI4NDgyNDh9.Kd0kiPErlp-Oa5LfDmisC_vQsLcVYs6jQFmkM2Jg5Pk";
+
+export const top10route = "https://quiz-leaderboard-58cf6.nn.r.appspot.com/scores/top10";
