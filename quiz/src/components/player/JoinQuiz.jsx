74b5832faceb6316aed0c2ab512931e9645aa048
Vladimir <le0679993@crc-lennox.qc.ca>
2023-09-14 09:35:06 -0400

    useState variables for JoinQuiz

--- quiz/src/components/player/JoinQuiz.jsx
+++ quiz/src/components/player/JoinQuiz.jsx
@@ -1,7 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function JoinQuiz() {
+  const [playerName, setPlayerName] = useState(""); // Define the playerName state
+  const [code, setCode] = useState("")
+
+  const handleNameChange = (e) => {
+    setPlayerName(e.target.value); // Update the playerName state when input changes
+  };
+  
+  const handleCodeChange = (e) => {
+    setCode(e.target.value); // Update the playerName state when input changes
+  };
+
   return (
     <div
       style={{ minHeight: "100vh" }}
@@ -9,16 +20,28 @@ function JoinQuiz() {
     >
       <div className="container m-5">
         <div className="row m-5">
-          <h1 className='text-center display-1'>Kahoot!</h1>
+          <h1 className="text-center display-1">Kahoot!</h1>
         </div>
         <div className="row m-4">
           <div className="col">
-            <input className='bg-light rounded-2 border-0 p-2' type="text" placeholder="Room code" />
+            <input
+              className="bg-light rounded-2 border-0 p-2"
+              type="text"
+              placeholder="Room code"
+              value= {code} // Set input value to the playerName state
+              onChange={handleCodeChange} // Call handleInputChange on input change
+            />
           </div>
         </div>
         <div className="row m-4">
           <div className="col">
-            <input className='bg-light rounded-2 border-0 p-2' type="text" placeholder="Username"/>
+            <input
+              className="bg-light rounded-2 border-0 p-2"
+              type="text"
+              placeholder="Username"
+              value={playerName} // Set input value to the playerName state
+              onChange={handleNameChange} // Call handleInputChange on input change
+            />
           </div>
         </div>
         <div className="d-flex justify-content-center">
@@ -27,15 +50,23 @@ function JoinQuiz() {
               style={{ color: "inherit", textDecoration: "none" }}
               to="/multiplayer/waitingRoomStudent"
             >
-              <button type="submit" className="px-2 py-1 btn btn-lg btn-secondary rounded-pill">
+              <button
+                type="submit"
+                className="px-2 py-1 btn btn-lg btn-secondary rounded-pill"
+              >
                 Join Room
               </button>
             </Link>
           </div>
           <div className="m-2">
-            <Link style={{color: "inherit",textDecoration: "none"}}
-                  to="/multiplayer/choice">
-              <button type="button" className="px-2 py-1 btn btn-lg btn-secondary rounded-pill">
+            <Link
+              style={{ color: "inherit", textDecoration: "none" }}
+              to="/multiplayer/choice"
+            >
+              <button
+                type="button"
+                className="px-2 py-1 btn btn-lg btn-secondary rounded-pill"
+              >
                 Go Back
               </button>
             </Link>
@@ -43,7 +74,7 @@ function JoinQuiz() {
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default JoinQuiz;
