2e03b6a57875f0ead29a63f811b9b3b8341783c4
Vladimir <le0679993@crc-lennox.qc.ca>
2023-09-14 09:26:15 -0400

    centered the WaitingRoomStudent

--- quiz/src/App.jsx
+++ quiz/src/App.jsx
@@ -1,29 +1,30 @@
 import { Link } from "react-router-dom";
-function App() {
+
+function WaitingRoomStudent() {
   return (
     <div
       style={{ minHeight: "100vh" }}
-      className="d-flex p-5 text-center  justify-content-center align-items-center "
+      className="d-flex flex-column justify-content-center align-items-center"
     >
-      <div>
-        <h1>Welcome to Quiz Application!</h1>
-        <h5 className=" py-5 ">Made by Vladimir and Munir.</h5>
-        <div>
-          <Link
-            style={{ color: "inherit", textDecoration: "none" }}
-            to="/multiplayer"
-          >
-            {" "}
-            <button
-              type="button"
-              className="px-5 p-3 btn btn-lg btn-secondary rounded-pill"
-            >
-              Play{" "}
-            </button>
-          </Link>
+      
+      <div className="container m-5 text-center">
+        <div className="row m-5">
+          <h1 className='text-center display-1'>Kahoot!</h1>
+        </div>
+        <div className="row m-4">
+          <div className="col">
+            <h1 className='text-center display-3'>Welcome Vlad</h1>
+          </div>
+        </div>
+        <div className="row m-4">
+          <div className="col">
+            <h1 className='text-center display-3'>Waiting for host to start the game.</h1>
+          </div>
         </div>
       </div>
+      
     </div>
-  );
+  )
 }
-export default App;
+
+export default WaitingRoomStudent;
--- quiz/src/components/player/WaitingRoomStudent.jsx
+++ quiz/src/components/player/WaitingRoomStudent.jsx
@@ -1,17 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function WaitingRoomStudent() {
   return (
-    <div>
-      <div className="col d-md-flex justify-content-md-end">
-        <Link style={{color: "inherit",textDecoration: "none"}}
-            to="/multiplayer/joinQuiz">
-              <button type="button" className="px-5 p-3 m-3 btn btn-lg btn-secondary rounded-pill">
-                  Go Back
-              </button>
-          </Link>
-      </div>
+    <div
+      style={{ minHeight: "100vh" }}
+      className="d-flex flex-column justify-content-center align-items-center"
+    >
+      
       <div className="container m-5 text-center">
         <div className="row m-5">
           <h1 className='text-center display-1'>Kahoot!</h1>
@@ -27,8 +23,21 @@ function WaitingRoomStudent() {
           </div>
         </div>
       </div>
+      <div className="col d-md-flex justify-content-md-end">
+        <Link
+          style={{ color: "inherit", textDecoration: "none" }}
+          to="/multiplayer/joinQuiz"
+        >
+          <button
+            type="button"
+            className="px-5 p-3 m-3 btn btn-lg btn-secondary rounded-pill"
+          >
+            Go Back
+          </button>
+        </Link>
+      </div>
     </div>
   )
 }
 
-export default WaitingRoomStudent
+export default WaitingRoomStudent;
