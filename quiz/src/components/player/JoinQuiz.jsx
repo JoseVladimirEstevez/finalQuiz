5b6d41ab0a442133656f293d0cb196bbabacb67d
Vladimir <le0679993@crc-lennox.qc.ca>
2023-09-14 09:19:58 -0400

    centered the JoinQuiz

--- quiz/src/components/player/JoinQuiz.jsx
+++ quiz/src/components/player/JoinQuiz.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function JoinQuiz() {
   return (
-    <div>
-      <div className="container m-5 text-center">
+    <div
+      style={{ minHeight: "100vh" }}
+      className="d-flex flex-column p-5 text-center justify-content-center align-items-center"
+    >
+      <div className="container m-5">
         <div className="row m-5">
           <h1 className='text-center display-1'>Kahoot!</h1>
         </div>
@@ -18,29 +21,29 @@ function JoinQuiz() {
             <input className='bg-light rounded-2 border-0 p-2' type="text" placeholder="Username"/>
           </div>
         </div>
-        <div className="row m-5">
-          <div className="col">
+        <div className="d-flex justify-content-center">
+          <div className="m-2">
             <Link
               style={{ color: "inherit", textDecoration: "none" }}
               to="/multiplayer/waitingRoomStudent"
             >
-              <button className='px-5 p-3 btn btn-lg btn-secondary rounded-pill'>Enter Room</button>
+              <button type="submit" className="px-2 py-1 btn btn-lg btn-secondary rounded-pill">
+                Join Room
+              </button>
             </Link>
           </div>
-          <div className="col">
-              <Link style={{color: "inherit",textDecoration: "none"}}
+          <div className="m-2">
+            <Link style={{color: "inherit",textDecoration: "none"}}
                   to="/multiplayer/choice">
-                    <button type="button" className="px-5 p-3 btn btn-lg btn-secondary rounded-pill">
-                        Go Back
-                    </button>
-              </Link>
+              <button type="button" className="px-2 py-1 btn btn-lg btn-secondary rounded-pill">
+                Go Back
+              </button>
+            </Link>
           </div>
         </div>
       </div>
-
-
     </div>
   )
 }
 
-export default JoinQuiz
+export default JoinQuiz;
